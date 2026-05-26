d92f964bdc683e707f11b0cf62ddd6f0d88591e9
Luiz <luiz22gcs@gmail.com>
2026-05-26 14:14:00 -0300

    feat:view para materiais

--- backend/disciplinas/views.py
+++ backend/disciplinas/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.generics import ListCreateAPIView
+from disciplinas.models import Material
 
-# Create your views here.
+
+class MaterialCreateListView(generics,ListCreateAPIView):
+    queryset = Material.objects.all()
+    serializer_class = None
+
+class MaterialRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Material.objects.all()
+    serializer_class = None
