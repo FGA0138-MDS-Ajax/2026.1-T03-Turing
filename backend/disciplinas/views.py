536204586c61558b8d58be8684eb1df123ef5e03
Luiz <luiz22gcs@gmail.com>
2026-05-26 14:32:55 -0300

    feat: Serializer adicionada a view

--- backend/disciplinas/views.py
+++ backend/disciplinas/views.py
@@ -1,12 +1,13 @@
 from rest_framework import generics
 from rest_framework.generics import ListCreateAPIView
 from disciplinas.models import Material
+from disciplinas.serializers import MaterialSerializer
 
 
 class MaterialCreateListView(generics,ListCreateAPIView):
     queryset = Material.objects.all()
-    serializer_class = None
+    serializer_class = MaterialSerializer
 
 class MaterialRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Material.objects.all()
-    serializer_class = None
+    serializer_class = MaterialSerializer
