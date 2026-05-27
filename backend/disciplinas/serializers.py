ce452a5da6263b1c6c04c225bf3f9477c692b771
Clarice <gussonclarice@gmail.com>
2026-05-27 18:36:30 -0300

    feat: criação da serializer de Disciplina e DisciplinaPrerequisito

--- backend/disciplinas/serializers.py
+++ backend/disciplinas/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Conteudo
+from .models import Conteudo, Disciplina, DisciplinaPrerequisito
 
 class ConteudoSerializer(serializers.ModelSerializer):
     class Meta:
@@ -7,3 +7,23 @@ class ConteudoSerializer(serializers.ModelSerializer):
         fields = '__all__'
         #disciplina já é obrigatorio pq é FK sem null=True
         
+class DisciplinaPrerequisitoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DisciplinaPrerequisito
+        fields = '__all__'
+
+class DisciplinaSerializer(serializers.ModelSerializer):
+    prerequisitos_detalhados = serializers.SerializerMethodField()
+    # Exibe a lista de pré-requisitos ao dar um GET
+
+    class Meta:
+        model = Disciplina
+        fields = ['id', 'nome', 'descricao', 'data_create', 'data_update', 'prerequisitos_detalhados']
+
+    def get_prerequisitos_detalhados(self, obj):
+        # Busca as disciplinas que são pré-requisito dessa
+        lista_prerequisitos = obj.prerequisitos.all()
+        return [
+            {"id": item.prerequisito.id, "nome": item.prerequisito.nome}
+            for item in lista_prerequisitos
+        ]
