f4b533c227ecd237ba8499a6626289bd857ef576
Luiz <luiz22gcs@gmail.com>
2026-05-27 15:12:19 -0300

    feat: viwes corrigidas

--- backend/disciplinas/views.py
+++ backend/disciplinas/views.py
@@ -2,12 +2,15 @@ from rest_framework import generics
 from rest_framework.generics import ListCreateAPIView
 from disciplinas.models import Material
 from disciplinas.serializers import MaterialSerializer
+from disciplinas.permissions import IsGoStudyProfOrAdmin
 
 
 class MaterialCreateListView(generics.ListCreateAPIView):
     queryset = Material.objects.all()
     serializer_class = MaterialSerializer
+    permission_classes = [IsGoStudyProfOrAdmin]
 
 class MaterialRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Material.objects.all()
     serializer_class = MaterialSerializer
+    permission_classes = [IsGoStudyProfOrAdmin]
