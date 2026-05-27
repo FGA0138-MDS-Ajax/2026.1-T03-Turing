d811e70d438b35082f2c9d916ed3eea338c6c7c8
Thiago Henrique <estudos.trabalhos12@gmail.com>
2026-05-27 02:17:01 -0300

    feat: adiciona serializers matricula

---
+++ backend/turmas/serializers.py
@@ -0,0 +1,25 @@
+
+from rest_framework import serializers
+from .models import Matricula
+
+class MatriculaSerializer(serializers.BaseSerializer):
+
+    class Meta:
+        model = Matricula
+
+        fields =['id','aluno','conteudo','matriculado_em']
+
+        #campos que o front pode apenas ler (GET)
+        read_only_fields = ['id','matriculado_em']
+
+
+    def to_representation(self, instance):
+
+        representacao = super().to_representation(instance)
+
+        representacao["detalhes_conteudo"]={
+            "id": instance.conteudo.id,
+            "nome": instance.conteudo.nome,
+            "descricao": instance.conteudo.descricao
+        }
+        return representacao
