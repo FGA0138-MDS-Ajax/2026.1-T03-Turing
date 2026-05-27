9f164cd2183e000239d0ebc09bea65caf1902f4e
Clarice <gussonclarice@gmail.com>
2026-05-27 18:44:14 -0300

    feat: configurando EndPoints de Disciplinas nas urls

--- backend/disciplinas/urls.py
+++ backend/disciplinas/urls.py
@@ -2,10 +2,12 @@ from django.urls import path, include
 from django.contrib import admin
 
 from rest_framework.routers import DefaultRouter
-from .views import ConteudoViewSet
+from .views import ConteudoViewSet, DisciplinaViewSet, DisciplinaPrerequisitoViewSet
 
 router = DefaultRouter()
 router.register(r'conteudos', ConteudoViewSet, basename='conteudo')
+router.register(r'disciplinas', DisciplinaViewSet, basename='disciplina')
+router.register(r'prerequisitos', DisciplinaPrerequisitoViewSet, basename='prerequisito')
 
 urlpatterns = [
     path('', include(router.urls)),
