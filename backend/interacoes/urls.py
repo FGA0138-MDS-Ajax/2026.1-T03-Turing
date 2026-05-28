6440c79d8e32a75faef6d3ac5b09bbc163a3f040
luisa5r <luisa5lvly@gmail.com>
2026-05-28 01:18:24 -0300

    adiciona urls.py e configura url pattern

---
+++ backend/interacoes/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import InscricaoViewSet
+
+router = DefaultRouter()
+
+router.register(r'inscricoes', InscricaoViewSet, basename='inscricao')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
