97e5973e1d64c50ae03604a01d065c88992d6ef9
luisa5r <luisa5lvly@gmail.com>
2026-05-28 03:07:03 -0300

    adiciona logica para aprovacao de inscricao

--- backend/interacoes/views.py
+++ backend/interacoes/views.py
@@ -3,6 +3,10 @@ from rest_framework.permissions import IsAuthenticated
 from .models import Inscricao
 from .serializers import InscricaoSerializer
 from usuarios.permissions import IsGoStudyAdmin
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
+from django.utils import timezone
 
 class InscricaoViewSet(viewsets.ModelViewSet):
     queryset = Inscricao.objects.all()
@@ -17,3 +21,25 @@ class InscricaoViewSet(viewsets.ModelViewSet):
             queryset = queryset.filter(status=status_param)
             
         return queryset
+    
+    @action(detail=True, methods=['patch'])
+    def aprovar(self, request, pk=None):
+        inscricao = self.get_object()
+
+        if inscricao.status != 'pendente':
+            return Response(
+            {'erro': 'essa inscrição ja foi analisada'},
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+        inscricao.status = 'aprovado'
+        inscricao.analisado_por = request.user.admin
+        inscricao.analisado_em = timezone.now()
+        perfil = inscricao.professor.perfil
+        perfil.is_active = True
+        perfil.save()
+
+        return Response(
+            {'mensagem': 'Inscrição aprovada com sucesso'},
+            status=status.HTTP_200_OK
+        )
