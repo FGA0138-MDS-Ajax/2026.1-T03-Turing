0e6d83b19892e444a1c5ea1e2745b8fcbb91a077
luisa5r <luisa5lvly@gmail.com>
2026-05-29 17:26:23 -0300

    adiciona estrutura de email para boas vindas de professor

--- backend/services/email_service.py
+++ backend/services/email_service.py
@@ -9,3 +9,18 @@ def enviar_email(assunto, mensagem, destinatario):
         recipient_list=[destinatario],
         fail_silently=False
     )
+
+def enviar_email_boas_vindas_professor(nome, email):
+    assunto='Cadastro de professor recebido - GoStudy',
+    mensagem=f'''
+Olá, {nome}!
+Seu cadastro foi realizado com sucesso.
+Seu currículo foi enviado para análise da equipe administrativa da GoStudy e sua inscrição está atualmente com status pendente.
+Você receberá uma nova notificação quando sua solicitação for aprovada ou recusada.
+Por favor, aguarde até 3 dias úteis para a revisão da sua inscrição.
+
+Atenciosamente,
+Equipe GoStudy
+'''
+    
+    enviar_email(assunto, mensagem, email)
