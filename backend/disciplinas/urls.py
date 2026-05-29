40bbc6fdebc7be5c55e67adeb1d54798e39e6244
Clarice <gussonclarice@gmail.com>
2026-05-29 19:43:30 -0300

    Implementando rotas de disciplinas nas urls

--- backend/disciplinas/urls.py
+++ backend/disciplinas/urls.py
@@ -2,10 +2,11 @@ from django.urls import path, include
 from django.contrib import admin
 
 from rest_framework.routers import DefaultRouter
-from .views import ConteudoViewSet
+from .views import ConteudoViewSet, DisciplinaViewSet
 
 router = DefaultRouter()
 router.register(r'conteudos', ConteudoViewSet, basename='conteudo')
+router.register(r'', DisciplinaViewSet, basename='disciplina')
 
 urlpatterns = [
     path('', include(router.urls)),
