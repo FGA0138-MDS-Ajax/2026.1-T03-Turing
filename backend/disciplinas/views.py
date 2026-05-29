25a93c8aee46299ed50dbbdae0ba54d6098c2565
Clarice <gussonclarice@gmail.com>
2026-05-29 19:44:40 -0300

    Adicionando ViewSet para permissões e CRUD

--- backend/disciplinas/views.py
+++ backend/disciplinas/views.py
@@ -1,8 +1,8 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
-from .models import Conteudo
-from .serializers import ConteudoSerializer
+from .models import Conteudo, Disciplina
+from .serializers import ConteudoSerializer, DisciplinaSerializer
 from usuarios.permissions import IsGoStudyAdmin
 
 class ConteudoViewSet(viewsets.ModelViewSet):
@@ -15,4 +15,16 @@ class ConteudoViewSet(viewsets.ModelViewSet):
             return [IsGoStudyAdmin()]
         
         # GET ou visualizar só precisa estar logado
+        return [IsAuthenticated()]
+
+class DisciplinaViewSet(viewsets.ModelViewSet):
+    queryset = Disciplina.objects.all()
+    serializer_class = DisciplinaSerializer
+
+    def get_permissions(self):
+        # Apenas admin pode criar (create), editar (update/partial_update) e deletar (destroy)
+        if self.action in ['create', 'update', 'partial_update', 'destroy']:
+            return [IsGoStudyAdmin()]
+        
+        # Alunos e professores autenticados podem apenas visualizar (list, retrieve)
         return [IsAuthenticated()]
