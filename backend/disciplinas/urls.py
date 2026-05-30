c1f8c50e0d9deffa809451e29bc8fcd1a960f36a
gabriel-otacilio <gabriel.otacilio135@gmail.com>
2026-05-29 21:33:33 -0300

    fix: ajustanto erro que o gabriel burrinho fez

--- backend/disciplinas/urls.py
+++ backend/disciplinas/urls.py
@@ -3,13 +3,13 @@ from .views import MaterialCreateListView, MaterialRetrieveUpdateDestroyAPIView
 from django.contrib import admin
 from rest_framework.routers import DefaultRouter
 from .views import ConteudoViewSet
-
+router = DefaultRouter()
+router.register(r'conteudos', ConteudoViewSet, basename='conteudo')
 urlpatterns = [
     path('materiais/', MaterialCreateListView.as_view(), name='materiais-create-view'),
     path('materiais/<int:pk>/', MaterialRetrieveUpdateDestroyAPIView.as_view(), name='materiais-detail-view'),
     path('', include(router.urls)),
 ]
 
-router = DefaultRouter()
-router.register(r'conteudos', ConteudoViewSet, basename='conteudo')
+
 
