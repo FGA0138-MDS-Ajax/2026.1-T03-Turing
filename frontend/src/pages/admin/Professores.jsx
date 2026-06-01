4a1cf01fe445d39dfd8dfd7a1fe5a60c7c661bf9
gabriel-otacilio <gabriel.otacilio135@gmail.com>
2026-06-01 00:36:40 -0300

    fix: ajustanto bug do inferno de ajuste de curriculo e inscricao

--- frontend/src/pages/admin/Professores.jsx
+++ frontend/src/pages/admin/Professores.jsx
@@ -1,10 +1,12 @@
-import { AdminLayout } from '../../components/admin/AdminLayout';
+import {AdminLayout} from '../../components/admin/AdminLayout';
 import GerenciamentoProfessores from '../Admin/Professores/professores';
+import TeacherReview from '../admin/TeacherReview/TeacherReview';
 
 export function Professores() {
-  return (
-    <AdminLayout>
-      <GerenciamentoProfessores />
-    </AdminLayout>
-  );
+    return (
+        <AdminLayout>
+            <GerenciamentoProfessores/>
+            <TeacherReview/>
+        </AdminLayout>
+    );
 }
