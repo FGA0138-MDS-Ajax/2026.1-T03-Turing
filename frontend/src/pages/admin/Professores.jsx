22f3c2a9c27ad3d8434221375d85b9a3592d15f3
mejg <mariaeduardajg2401@gmail.com>
2026-05-31 22:58:51 -0300

    feat: Integração com ps-professores

--- frontend/src/pages/admin/Professores.jsx
+++ frontend/src/pages/admin/Professores.jsx
@@ -1,10 +1,12 @@
 import { AdminLayout } from '../../components/admin/AdminLayout';
 import GerenciamentoProfessores from '../Admin/Professores/professores';
+import TeacherReview from '../Admin/TeacherReview/TeacherReview';
 
 export function Professores() {
   return (
     <AdminLayout>
       <GerenciamentoProfessores />
+      <TeacherReview />
     </AdminLayout>
   );
 }
