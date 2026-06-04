c30dd1fe5b2e1b6d2debcaf64db4c6f2d756d089
Thiago Henrique <estudos.trabalhos12@gmail.com>
2026-06-04 17:35:13 -0300

    feat: configura urls

--- backend/disciplinas/urls.py
+++ backend/disciplinas/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path, include
-from .views import MaterialCreateListView, MaterialRetrieveUpdateDestroyAPIView
 from django.contrib import admin
 from rest_framework.routers import DefaultRouter
-from .views import ConteudoViewSet, DisciplinaViewSet
-
+from .views import ConteudoViewSet, DisciplinaViewSet, MaterialViewSet
 router = DefaultRouter()
 router.register(r'conteudos', ConteudoViewSet, basename='conteudo')
 router.register(r'', DisciplinaViewSet, basename='disciplina')
+router.register(r'disciplinas/materiais', MaterialViewSet, basename='materiais')
+
 
 urlpatterns = [
-    path('materiais/', MaterialCreateListView.as_view(), name='materiais-create-view'),
-    path('materiais/<int:pk>/', MaterialRetrieveUpdateDestroyAPIView.as_view(), name='materiais-detail-view'),
+    #path('materiais/', MaterialCreateListView.as_view(), name='materiais-create-view'),
+    #path('materiais/<int:pk>/', MaterialRetrieveUpdateDestroyAPIView.as_view(), name='materiais-detail-view'),
     path('', include(router.urls)),
 ]
 
